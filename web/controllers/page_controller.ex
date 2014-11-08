325737771edcc4de81c6311f3f0723e180082663
Jared Bunting <jared.bunting@peachjean.com>
2014-11-08 10:44:38 -0600

    Getting the token from eventbrite.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,7 @@
 defmodule CodeForConduct.PageController do
   use Phoenix.Controller
   import HTTPoison
+  import Poison
 
   plug :action
 
@@ -8,20 +9,32 @@ defmodule CodeForConduct.PageController do
     render conn, "index"
   end
 
-  def auth(conn, %{"code" => code}) do
-    IO.puts "i got a code " <> code
+  defp get_token(code) do
     body = "code=#{code}&grant_type=authorization_code&client_secret=CDRPVW63YBXLHSPGL4Q25AFJKTXC7EYWOJ6LOQEE3UDIOABT6N&client_id=7EBFEB24BRVOV6J5KG"
     case HTTPoison.post("https://www.eventbrite.com/oauth/token", body, [{<<"Content-Type">>, <<"application/x-www-form-urlencoded">>}]) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts "BODY :: [[#{body}]]"
-      {:ok, %HTTPoison.Response{status_code: 404, body: body}} ->
-        IO.puts "Not found :-( #{body}"
+      {:ok, %HTTPoison.Response{status_code: 200, body: body, }} ->
+        case Poison.decode body do
+        {:ok, %{"access_token" => token}} ->
+          {:ok, token}
+        {:error, r} ->
+          raise r
+        end
       {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
-        IO.puts "I got a different code #{status_code} -- what the hell? #{body}"
+        raise "Got #{status_code} (#{body})..."
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
+        raise reason
     end
-    render conn, "auth_test", code: code
+  end
+    
+
+  def auth(conn, %{"code" => code}) do
+    IO.puts "i got a code " <> code
+    case get_token(code) do
+      {:ok, token} ->
+        render conn, "auth_test", token: token
+      {:error, r} ->
+        raise r
+    end
   end
 
   def auth(conn, _params) do
