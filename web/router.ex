510b52124daaa7241f7c35d40bd3fcd4a41ac5c1
cory.hughes <cory.hughes@digitalreasoning.com>
2014-11-08 20:14:05 -0600

    Fuck heroku

--- web/router.ex
+++ web/router.ex
@@ -2,15 +2,7 @@ defmodule CodeForConduct.Router do
   use Phoenix.Router
 
   scope "/" do
-    # Use the default browser stack.
-    pipe_through :browser
-
     get "/", CodeForConduct.PageController, :index, as: :pages
     get "/auth", CodeForConduct.PageController, :auth, as: :pages
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api" do
-  #   pipe_through :api
-  # end
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,21 +1,3 @@
 defmodule CodeForConduct.PageView do
-  use Phoenix.View, root: "web/templates"
 
-  # The quoted expression returned by this block is applied
-  # to this module and all other views that use this module.
-  using do
-    quote do
-      # Import common functionality
-      import CodeForConduct.I18n
-      import CodeForConduct.Router.Helpers
-
-      # Use Phoenix.HTML to import all HTML functions (forms, tags, etc)
-      use Phoenix.HTML
-
-      # Common aliases
-      alias Phoenix.Controller.Flash
-    end
-  end
-
-  # Functions defined here are available to all other views/templates
 end
